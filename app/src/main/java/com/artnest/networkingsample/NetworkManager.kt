e8354f4fd77afa65ff16a2967b0789a3b3b3eb16
artnest <artnest789@gmail.com>
2019-10-14 01:28:31 +0300

    Add OkHttp synchronous sample code

--- app/src/main/java/com/artnest/networkingsample/NetworkManager.kt
+++ app/src/main/java/com/artnest/networkingsample/NetworkManager.kt
@@ -1,12 +1,15 @@
 package com.artnest.networkingsample
 
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
 import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
 
 object NetworkManager {
 
-    fun connectToUrl(httpUrl: String): String {
+    fun openUrlWithHttpURLConnection(httpUrl: String): String {
         val url = URL(httpUrl)
         val urlConnection = (url.openConnection() as HttpURLConnection).apply {
             readTimeout = 10000 // ms
@@ -25,5 +28,31 @@ object NetworkManager {
 
     private fun read(inputStream: InputStream, len: Int): String {
         return inputStream.readBytes().toString(Charsets.UTF_8)
+    }
+
+    fun openUrlWithOkHttp(url: String): String {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .get()
+            .url(url)
+            .build()
+
+        return try {
+            client.newCall(request).execute().use { response ->
+                response.body!!.string()
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            e.localizedMessage!!
+        }
+
+        /*val call = client.newCall(request)
+        try {
+            val response = call.execute()
+            val content = response.body!!.string()
+            response.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }*/
     }
 }
